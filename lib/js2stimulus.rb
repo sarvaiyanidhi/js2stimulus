1f4c64f950a381f85dd13340e44cb0dccdd738a9
Nidhi Sarvaiya <sarvaiya.nidhi@gmail.com>
2023-03-22 22:41:25 -0400

    logic to convert JS code to Stimulus.js

--- lib/js2stimulus.rb
+++ lib/js2stimulus.rb
@@ -1,8 +1,16 @@
 # frozen_string_literal: true
 
 require_relative "js2stimulus/version"
+require 'execjs'
 
 module Js2stimulus
   class Error < StandardError; end
-  # Your code goes here...
+  def self.convert(js_code)
+    context = ExecJS.compile('var stimulus = { controllers: [] };')
+
+    # Replace this line with your code to convert the JavaScript code to Stimulus.js format
+    converted_code = js_code.gsub(/this\.foo/, 'stimulus.controllers[0].foo')
+
+    context.eval(converted_code)
+  end
 end
