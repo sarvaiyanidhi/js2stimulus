7d885d2408cef30687e2efbf7d8dfa69dab7bc10
Nidhi Sarvaiya <sarvaiya.nidhi@gmail.com>
2023-03-23 18:39:24 -0400

    Updated test cases

--- test/js2stimulus_test.rb
+++ test/js2stimulus_test.rb
@@ -1,15 +1,13 @@
 # frozen_string_literal: true
 
 require "test_helper"
+require 'js2stimulus'
 
 class Js2stimulusTest < Test::Unit::TestCase
-  test "VERSION" do
-    assert do
-      ::Js2stimulus.const_defined?(:VERSION)
-    end
-  end
+  def test_convert
+    js_code = 'this.foo = "bar";'
+    expected_output = 'stimulus.controllers[0].foo = "bar";'
 
-  test "something useful" do
-    assert_equal("expected", "actual")
+    assert_equal expected_output, Js2stimulus.convert(js_code)
   end
 end
